d4839152ee6ffa22338cb519924a6094d6b52f7b
FATIH <fatihkokay@live.com>
2025-12-25 09:55:09 +0300

    edit

--- Models/TenderModel.cs
+++ Models/TenderModel.cs
@@ -12,7 +12,6 @@ namespace TenderFlow.Models
         public int Id { get; set; }
 
         public int FirmId { get; set; }
-<<<<<<< HEAD
         public string FirmName { get; set; } = string.Empty;
         public string TenderCode { get; set; } = string.Empty;
         public string TenderName { get; set; } = string.Empty;
@@ -20,26 +19,12 @@ namespace TenderFlow.Models
         public string PublicAuthorityName { get; set; } = string.Empty;
         public string TenderType { get; set; } = string.Empty;
         public string TenderMethod { get; set; } = string.Empty;
-=======
-        public string? FirmName { get; set; }
-
-        public string TenderCode { get; set; } = string.Empty;
-        public string TenderName { get; set; } = string.Empty;
-
-        public string PublicAuthorityCode { get; set; } = string.Empty;
-        public string? PublicAuthorityName { get; set; }
-
-        public string TenderType { get; set; } = string.Empty;
-        public string TenderMethod { get; set; } = string.Empty;
-
->>>>>>> a52d2246997c9f5b7a8ea32d1ef7c5d2ef2c0fd7
         public DateTime TenderStartDate { get; set; }
         public DateTime TenderEndDate { get; set; }
         public int TenderDueDate { get; set; }
 
         public decimal TenderQuantity { get; set; }
         public decimal TenderAmount { get; set; }
-<<<<<<< HEAD
         public string Currency { get; set; } = string.Empty;
         public int? TemporaryGuaranteeRateId { get; set; }
         public string TemporaryGuaranteeSubject { get; set; } = string.Empty;
@@ -47,20 +32,8 @@ namespace TenderFlow.Models
         public string? FinalGuaranteeSubject { get; set; }
         public DateTime AnnouncementDate { get; set; }
         public string TenderStatus { get; set; } = string.Empty;
-=======
-
-        public string Currency { get; set; } = "TRY";
-        public int? TemporaryGuaranteeRateId { get; set; }
-        public string? TemporaryGuaranteeSubject { get; set; }
-
-        public int? FinalGuaranteeRateId { get; set; }
-        public string? FinalGuaranteeSubject { get; set; }
-        public DateTime AnnouncementDate { get; set; }
->>>>>>> a52d2246997c9f5b7a8ea32d1ef7c5d2ef2c0fd7
         public DateTime? DocumentUploadDate { get; set; }
         public DateTime? ContractDate { get; set; }
-        public string? TenderStatus { get; set; }
-
         public DateTime CreatedAt { get; set; }
         [NotMapped]
         public List<TenderDeviceModel> Devices { get; set; } = new();
